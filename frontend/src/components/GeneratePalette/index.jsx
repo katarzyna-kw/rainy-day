eaf0f6af008d3eff2557b3b5d5799f035db2341a
katarzyna-kw <katarzyna.koch@gmail.com>
2022-04-20 16:04:36 -0500

    refactored for cleaner code

--- frontend/src/components/GeneratePalette/index.jsx
+++ frontend/src/components/GeneratePalette/index.jsx
@@ -3,24 +3,15 @@ import SavePaletteForm from '../SavePaletteForm/'
 
 function GeneratePalette({currentPalette, title}) {
 
-  console.log("current in generate: ", currentPalette)
-
   return (
     <div className='generate-palette__container'>
       <h2>{title} Palette</h2>
       <div className='palette__container'>
-        <div style={{backgroundColor: `${currentPalette[0].value}`}}className='palette-color'>
-          {currentPalette[0].value}
-        </div>
-        <div style={{backgroundColor: `${currentPalette[1].value}`}}className='palette-color'>
-          {currentPalette[1].value}
-        </div>
-        <div style={{backgroundColor: `${currentPalette[2].value}`}}className='palette-color'>
-          {currentPalette[2].value}
-        </div>
-        <div style={{backgroundColor: `${currentPalette[3].value}`}}className='palette-color'>
-          {currentPalette[3].value}
-        </div>
+        {currentPalette && currentPalette.map((color, i) => (
+          <div key={i} style={{backgroundColor: `${color.value}`}} className='palette-color'>
+            {color.value}
+          </div>
+        ))}
       </div>
       <SavePaletteForm currentPalette={currentPalette}/>
     </div>
