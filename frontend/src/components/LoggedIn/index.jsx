d0efa8d50d0f8abd3a96bee591cf33947a03ccf3
katarzyna-kw <katarzyna.koch@gmail.com>
2022-04-25 22:01:19 -0500

    landing pg if user exists but no fp/palettes

--- frontend/src/components/LoggedIn/index.jsx
+++ frontend/src/components/LoggedIn/index.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import apiCalls from "../../api/apiCalls"
 import MiniFontPairView from '../MiniFontPairView'
 import MiniPaletteView from "../MiniPaletteView.js"
+import createPalette from '../../assets/palette.gif'
+import createPair from '../../assets/font-pair.gif'
 
 function LoggedIn({user}) {
 
@@ -30,6 +32,7 @@ function LoggedIn({user}) {
 
   return (
     <div className="hp-info__container">
+      {(palettes && palettes.length > 0) && 
       <div className="hp-info__palettes">
         <h2 className="hp--subhead">My Latest Palette</h2>
         {(palettes && palettes.length > 0) ? <MiniPaletteView user={user} currentPalette={palettes[0]} /> : null}
@@ -37,13 +40,35 @@ function LoggedIn({user}) {
           <button className="btn btn-view-more">View More Palettes</button>
         </Link>
       </div>
+      }
+      {(fontPairs && fontPairs.length > 0) &&
       <div className="hp-info__font-pairs">
           <h2 className="hp--subhead">My Latest Font Pair</h2>
           {fontPairs ? <MiniFontPairView user={user} currentPair={fontPairs[0]} /> : null}
           <Link to={`/view-fonts`}>
-          <button className="btn btn-view-more">View More Font Pairs</button>
+            <button className="btn btn-view-more">View More Font Pairs</button>
           </Link>
         </div>
+      }
+      {
+      (palettes && palettes == 0) && 
+      <div className="hp-info__palettes how-to">
+        <Link to={`/create-color-palette`}>
+          <button className="btn">Create a color palette</button>
+        </Link>
+        <img className="how-to__img palette_img" src={createPalette} alt="Create a palette" />
+      </div> 
+      }
+      {
+      (fontPairs && fontPairs == 0) && 
+      <div className="hp-info__font-pairs how-to">
+        <Link to={`/create-font-pair`}>
+          <button className="btn">Create a font pair</button>
+        </Link>
+        <img className="how-to__img" src={createPair} alt="Create a font pair" />
+      </div> 
+      }
+
     </div>
   )
 }
